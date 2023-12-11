d7d1c098ae65e75a56b879d37faf5f8e313f43e5
Elliot <bitsch.elliot@gmail.com>
2023-12-11 14:28:36 +0100

    Improve build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,5 @@
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/space_chase.wasm
-cp -r assets out/assets
-cp index.html out/index.html
+wasm-bindgen --out-dir ./orbitale_release/ --target web ./target/wasm32-unknown-unknown/release/space_chase.wasm
+mkdir orbitale_release/assets/
+cp -r assets/* orbitale_release/assets/
+cp index.html orbitale_release/index.html
